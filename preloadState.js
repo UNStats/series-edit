663553bb50b772e7d89cd1286ec1704846511f52
Thilo Maier <git@maier.asia>
2018-01-11 16:14:06 -0500

    Updates

---
+++ preloadState.js
@@ -0,0 +1,34 @@
+window.preloadedState = {
+  series: {
+    id: "1035",
+    title: "Number of deaths attributed to chronic respiratory disease",
+    code: "SH_DTH_CRESPD"
+  },
+  dimensions: {
+    selected: [
+      {
+        id: "4",
+        name: "Nature",
+        selected: [
+          { id: "92", value: "C" },
+          { id: "93", value: "CA" },
+          { id: "94", value: "E" }
+        ],
+        selectable: []
+      },
+      {
+        id: "5",
+        name: "Sex",
+        selected: [{ id: "99", value: "Female" }, { id: "100", value: "Male" }],
+        selectable: []
+      },
+      {
+        id: "8",
+        name: "Units",
+        selected: [{ id: "102", value: "USD" }, { id: "103", value: "LCU" }],
+        selectable: []
+      }
+    ],
+    selectable: []
+  }
+};
