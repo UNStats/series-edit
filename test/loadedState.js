0f687b9ec4d72b10578626dd3d1d5b24d7bb6f3a
Thilo Maier <git@maier.asia>
2018-01-11 10:38:55 -0500

    Move test fixtures out of src

--- test/loadedState.js
+++ test/loadedState.js
@@ -11,32 +11,32 @@ export default {
         id: "4",
         name: "Nature",
         selected: [
-          {id: "92", value: "C"},
-          {id: "93", value: "CA"},
-          {id: "94", value: "E"}
+          { id: "92", value: "C" },
+          { id: "93", value: "CA" },
+          { id: "94", value: "E" }
         ],
         selectable: [
-          {id: "95", value: "G"},
-          {id: "96", value: "M"},
-          {id: "97", value: "N"},
-          {id: "98", value: "NA"}
+          { id: "95", value: "G" },
+          { id: "96", value: "M" },
+          { id: "97", value: "N" },
+          { id: "98", value: "NA" }
         ]
       },
       {
         id: "5",
         name: "Sex",
-        selected: [{id: "99", value: "Female"}, {id: "100", value: "Male"}],
-        selectable: [{id: "101", value: "Both"}]
+        selected: [{ id: "99", value: "Female" }, { id: "100", value: "Male" }],
+        selectable: [{ id: "101", value: "Both" }]
       },
       {
         id: "8",
         name: "Units",
-        selected: [{id: "102", value: "USD"}, {id: "103", value: "LCU"}],
+        selected: [{ id: "102", value: "USD" }, { id: "103", value: "LCU" }],
         selectable: [
-          {id: "104", value: "mgr/m^3"},
-          {id: "105", value: "h"},
-          {id: "106", value: "index"},
-          {id: "107", value: "Kg"}
+          { id: "104", value: "mgr/m^3" },
+          { id: "105", value: "h" },
+          { id: "106", value: "index" },
+          { id: "107", value: "Kg" }
         ]
       }
     ],
@@ -55,4 +55,4 @@ export default {
       }
     ]
   }
-}
+};
--- test/preloadedState.js
+++ test/preloadedState.js
@@ -11,25 +11,25 @@ export default {
         id: "4",
         name: "Nature",
         selected: [
-          {id: "92", value: "C"},
-          {id: "93", value: "CA"},
-          {id: "94", value: "E"}
+          { id: "92", value: "C" },
+          { id: "93", value: "CA" },
+          { id: "94", value: "E" }
         ],
         selectable: []
       },
       {
         id: "5",
         name: "Sex",
-        selected: [{id: "99", value: "Female"}, {id: "100", value: "Male"}],
+        selected: [{ id: "99", value: "Female" }, { id: "100", value: "Male" }],
         selectable: []
       },
       {
         id: "8",
         name: "Units",
-        selected: [{id: "102", value: "USD"}, {id: "103", value: "LCU"}],
+        selected: [{ id: "102", value: "USD" }, { id: "103", value: "LCU" }],
         selectable: []
       }
     ],
     selectable: []
   }
-}
+};
